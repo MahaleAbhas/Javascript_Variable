05e0628e2be97959cd6687c337ce97226e9133be
Abhas Sudhir Mahale <149258813+MahaleAbhas@users.noreply.github.com>
2025-03-01 07:00:56 +0000

    Variable

---
+++ Variable/Varible.js
@@ -0,0 +1,9 @@
+const accountId = 123654
+let accountName = "Abhas"
+var accountEmail = "Abhas@google.com"
+
+// var prefer not to use var because of issue in block scope and funtional scope
+
+
+// accountId = 212121 //value can't change beacause value is constant
+console.table([accountId, accountName, accountEmail])
